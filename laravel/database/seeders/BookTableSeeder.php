6f14783c3071b9c57bf578dc325ddc23b72e67d9
Tak-Kida <takumakida0@gmail.com>
2022-04-26 23:03:29 +0900

    booksのレコードを作成

---
+++ laravel/database/seeders/BookTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class BookTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'イシューからはじめよ 知的生産の「シンプルな本質」',
+            'author_id' => '1', // 安宅和人
+            'publisher_id' => '1', // 英治出版
+            'first_published' => '2010-11-01', // 2010-11-01
+        ];
+        DB::table('books')->insert($param);
+    }
+}
