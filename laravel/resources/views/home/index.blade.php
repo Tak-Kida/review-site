fe3fc6c4bfbcfe347b59a7766e001774128c9ce6
TakumaKida <takumakida0@gmail.com>
2022-05-13 00:30:37 +0900

    トップ画面を作成

---
+++ laravel/resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head></head>
+
+<body>
+    <h3>リンク一覧</h3>
+    <ul>
+        <li><a href="book/index">books</a></li>
+        <li><a href="author/index">authors</a></li>
+    </ul>
+</body>
+
+</html>
