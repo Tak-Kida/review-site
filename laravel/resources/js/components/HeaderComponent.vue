7f048897034619f59c8bfaf1ec140f95d337529e
TakumaKida <takumakida0@gmail.com>
2022-08-03 22:07:26 +0900

    ヘッダーを更新

--- laravel/resources/js/components/HeaderComponent.vue
+++ laravel/resources/js/components/HeaderComponent.vue
@@ -1,48 +1,58 @@
 <template>
     <header class="header">
-        <div class="header-left">
-            <div class="header-logo">
-                <small>logo</small>
+        <div class="header-container">
+            <div class="header-left">
+                <div class="header-logo">
+                    <small>logo</small>
+                </div>
+                <!-- サービス名 -->
+                <h2 class="service-title">Book-reviews</h2>
             </div>
-            <!-- サービス名 -->
-            <h2 class="service-title">Book-reviews</h2>
-        </div>
-        <div class="header-right">
-            <ul class="header-link-list">
-                <li class="header-link">ログイン</li>
-                <!-- <li class="header-link">USER</li>
-                <li class="header-link">ログアウト</li> -->
-                <!-- <ul>
-                    <li><router-link to="/">Top</router-link></li>
-                    <li><router-link to="/test-page01">Test Page01</router-link></li>
-                    <li><router-link to="/test-page02">Test Page02</router-link></li>
-                    <li><router-link to="/book">Book</router-link></li>
-                    <li><router-link to="/author">Author</router-link></li>
-                </ul> -->
-            </ul>
+            <div class="header-right">
+                <ul class="header-link-list">
+                    <li class="header-link">ログイン</li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
 
 <style>
 .header {
+    /* display: flex;
+    justify-content: space-between; */
+    width: 100%;
+    height: 72px;
+    background-color: #0c2e8f;
+}
+
+.header-container {
     display: flex;
     justify-content: space-between;
-    width: 100%;
-    height: 50px;
-    background-color: #0c2e8f;
+    width: 90%;
+    /* max-width: none; */
+    /* min-width: none; */
+    height: 100%;
+    margin: 0 auto;
+    /* background-color: green */
 }
 
 .header-left {
     display: flex;
-    /* width: 50px;
-    height: 50px; */
+    /* width: 50px; */
+    height: 100%;
     /* background-color: red */
+}
+
+.header-logo {
+    margin-left: 10px;
+    width: 60px;
+    background-color: darkred
 }
 
 .service-title {
     font-size: 40px;
-    margin: 5px 10px;
+    margin: 16px 10px;
     color:#fafafa;
 }
 
@@ -51,12 +61,6 @@
     justify-content: flex-end;
     width: 50%;
     /* background-color: blue */
-}
-
-.header-logo {
-    margin-left: 10px;
-    width: 50px;
-    background-color: darkred
 }
 
 .header-link-list {
@@ -70,7 +74,6 @@
     color:#fafafa;
     list-style: none;
     margin-right: 10px;
-    /* background-color: yellow */
 }
 
 </style>
