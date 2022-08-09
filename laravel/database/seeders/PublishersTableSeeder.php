5e3c03f4743ff0d1452a4147a575339b8b0643c5
TakumaKida <takumakida0@gmail.com>
2022-08-10 02:22:22 +0900

    publisherのseederを増量

--- laravel/database/seeders/PublishersTableSeeder.php
+++ laravel/database/seeders/PublishersTableSeeder.php
@@ -14,10 +14,28 @@ class PublishersTableSeeder extends Seeder
      */
     public function run()
     {
+        // #01
         $param = [
-            // 'id' => 1,
             'name' => '英治出版',
             'name_furigana' => 'えいじしゅっぱん',
+            'created_at' => '2022-05-01', // 2022-05-01
+            'updated_at' => '2022-05-01', // 2022-05-01
+        ];
+        DB::table('publishers')->insert($param);
+
+        // #02
+        $param = [
+            'name' => 'SBクリエイティブ',
+            'name_furigana' => 'えすびーくりえいてぃぶ',
+            'created_at' => '2022-05-01', // 2022-05-01
+            'updated_at' => '2022-05-01', // 2022-05-01
+        ];
+        DB::table('publishers')->insert($param);
+
+        // #03
+        $param = [
+            'name' => '東洋経済新報社',
+            'name_furigana' => 'とうようけいざいしんぽうしゃ',
             'created_at' => '2022-05-01', // 2022-05-01
             'updated_at' => '2022-05-01', // 2022-05-01
         ];
